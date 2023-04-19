a56d7012ef264509cec1d5692045ea2971f326c2
vitoananda <vtoananda@gmail.com>
2023-04-19 08:24:57 +0700

    added responsivity, animations, and fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,114 @@
 import Image from 'next/image';
-import avatar from '../public/avatar-vector.png'
+import avatar from '../public/avatar with bg.svg'
 import logo from '../public/vito ananda logo white.svg'
 import Head from 'next/head';
-import {BsDownload,BsLinkedin, BsInstagram} from 'react-icons/bs';
+import {BsDownload,BsLinkedin, BsInstagram,BsGithub} from 'react-icons/bs';
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
 
 
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+const variants = {
+  hidden: {opacity:0},
+  visible: {opacity:1, transition: {type:"tween",duration:1}}
+}
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Vito Rizqi Ananda Personal Website</title>
+        
       </Head>
       <main className='bg-white bg-cover'>
-        <section className=''>
-          <nav className='py-10 mb-12 flex justify-between px-10'>
-            <div className='px-2 py-2 fill-black bg-rd rounded-full drop-shadow-md' >
-            <Image className='h-7 w-7' src={logo}/>
+        {/* navbar */}
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-around px-0'>
+            <div className='px-2.5 py-2.5 bg-rd rounded-full drop-shadow-md' >
+            <Image className='h-7 w-7 fill-rd' src={logo}/>
             </div>
-            <a className='cursor-pointer'  href="../public/CV_Vito Rizqi Ananda.png" download>
-          <ul className='flex items-center  bg-rd px-3 py-1 rounded-md drop-shadow-md'>
+            <a className='cursor-pointer'  href="../public/CV_Vito Rizqi Ananda.png" download></a>
+          <ul className='flex items-center  bg-rd px-5 py-1 rounded-md drop-shadow-md'>
           <li>
-              <BsDownload className='cursor-pointer mr-2 text-white'/>
+              <BsDownload className='text-xl cursor-pointer mr-4 text-white'/>
             </li>
             <li>
-              <a className='text-lg text-white' href="#">Resume</a>
+              <a className='text-xl text-white' href="#">Resume</a>
             </li>
   
           </ul>
-          </a>
+          
           </nav>
-          <div className='text-center'>
-            <h2 className='font-bold text-4xl text-lbl pb-2'>Vito Rizqi Ananda</h2>
-            <h3 className='pb-4'>Developer and Graphic Designer.</h3>
-            <p className='pb-4 text-gray'>
-            a freelancer offering programming and design skills
-            </p>
-           <div className='text-3xl flex justify-center gap-1'>
-            <a href="https://www.linkedin.com/in/vito-rizqi-ananda/"><BsLinkedin className='mr-4 cursor-pointer'/></a>
+          {/* main page */}
+      
+<section className='h-full max-h-[640px] mb-8 xl:mb-24 px-10'>
+  <div className='flex flex-col lg:flex-row'>
+    <div className='lg:ml-8 xl:ml-[150px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1
+    px-4 lg:px-0'>
+      <motion.h2
+       variants={variants}
+       initial="hidden"
+       animate="visible" 
+       className='font-bold text-5xl text-lbl pb-5'>Vito Rizqi Ananda</motion.h2>
+       <motion.div 
+       variants={variants}
+       initial="hidden"
+       animate="visible" >
+        <TypeAnimation
+      sequence={[
+          'Developer',
+          2000,
+          'Graphic Designer',
+          2000
+        ]}
+        speed={50}
+        wrapper='span'
+        className='pb-1 text-xl'
+        repeat={Infinity}
+      />
+       </motion.div>
+      
+          <motion.p
+          variants={variants}
+          initial="hidden"
+          animate= {{opacity:1, transition: { type:"tween", duration:1.5}}}
+           className='pb-4 text-gray'>
+                a freelancer offering programming and design services
+            </motion.p>
+            <motion.ul
+            variants={variants}
+            initial="hidden"
+            animate={{opacity:1, transition: {duration:1.75}}} 
+             className='flex items-center text-3xl'>
+          <li>
+          <a href="https://www.linkedin.com/in/vito-rizqi-ananda/"><BsLinkedin className='mr-4 cursor-pointer'/></a>
+            </li>
+            <li>
             <a href="https://www.instagram.com/vitoanandq/"><BsInstagram className='mr-4 cursor-pointer'/></a>
-           </div>
-           <div className='relative mx-auto pt-10 w-80 h-80'>
-            <Image className='' src={avatar}/>
-           </div>
-          </div>
-          <div className='bg-rd bg-fill mt-2 p-5'>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit adipisci ipsum maiores soluta eligendi accusantium voluptatum tenetur, tempora, distinctio reiciendis laborum quis earum reprehenderit? Reprehenderit qui facilis unde necessitatibus?</p>
-
-          </div>
-         
+            </li>
+            <li>
+            <a href="https://github.com/vitoananda"><BsGithub className='mr-4 cursor-pointer'/></a>
+            </li>
+          </motion.ul>   
+    </div>
+    <motion.div
+    variants={variants}
+    initial="hidden"
+    animate="visible" 
+     className='hidden flex-1 lg:flex justify-end items-end'>
+                 <Image className='' src={avatar}/>
+               </motion.div>
+  </div>
         </section>
+        </section>
+        
      </main>
     </div>
     
